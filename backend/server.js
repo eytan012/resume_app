7dc935de3e31416dc378bffa90894ec4421ce8aa
eytan012 <eytan012@gmail.com>
2022-03-16 11:31:43 +0200

    setup backend && not found middleware

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import notFoundMiddleware from "./middleware/not-found.js";
+
+const app = express();
+
+app.get('/hey',(req,res)=>res.send("HELLO"))
+//middleware
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
