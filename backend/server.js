45a1b5eab410bdad0991bbba5377e055f5299efa
eytan012 <eytan012@gmail.com>
2022-03-18 12:14:13 +0200

    .env & error and not found middlewares

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
 import express from "express";
+import dotenv from 'dotenv'
+import errorHandlerMiddleWare from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 
 const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+	throw new Error("error");
+});
+
+
 
 //middleware
+app.use(errorHandlerMiddleWare);
 app.use(notFoundMiddleware);
+
 
 const port = process.env.PORT || 5000;
 
