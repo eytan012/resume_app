8185f63ad579472d8b9db112d0bd1b4f4d861561
eytan012 <eytan012@gmail.com>
2022-03-18 12:54:48 +0200

    vercel test

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,30 @@
 import express from "express";
-import dotenv from 'dotenv'
-import errorHandlerMiddleWare from "./middleware/error-handler.js";
-import notFoundMiddleware from "./middleware/not-found.js";
+import dotenv from "dotenv";
+import connectDB from "./db/connect.js";
 
 const app = express();
 dotenv.config();
 
-app.get("/", (req, res) => {
-	throw new Error("error");
-});
-
-
-
 //middleware
-app.use(errorHandlerMiddleWare);
+import errorHandlerMiddleWare from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleWare);
 
+app.get('/test',(req,res)=>{
+    res.send("WORKING")
+})
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+const start = async () => {
+	try {
+		await connectDB(process.env.DB_URI);
+		console.log("Connected to DB");
+		app.listen(port, () => console.log(`Server is running on port ${port}`));
+	} catch (error) {
+		console.log("Connection error =>", error);
+	}
+};
+
+start();
